9841d90d5b7db3df05298b471c04d765ae1719aa
s-appu <s-appu@users.noreply.github.com>
2017-11-30 17:18:59 -0500

    Utility to forbid termination via exit(0)

---
+++ src/main/java/com/core/DoNotTerminate.java
@@ -0,0 +1,20 @@
+static class DoNotTerminate {
+
+		public static class CannotExitException extends SecurityException {
+			private static final long serialVersionUID = 1379509680859956142L;
+		}
+
+		public static void forbidExit() {
+			final SecurityManager securityManager = new SecurityManager() {
+
+				@Override
+				public void checkPermission(final Permission permission) {
+					if (permission.getName().contains("exitVM")) {
+						throw new CannotExitException();
+					}
+				}
+			};
+			System.setSecurityManager(securityManager);
+		}
+
+	}
